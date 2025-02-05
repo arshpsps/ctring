4a7714a43c4e9ee6a313bb223330c4a9895b4598
arshpsps <arshh.gill@outlook.com>
2025-02-05 20:16:44 +0530

    fixed?

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
-main: src/main.c | builds
-	@ gcc -o builds/out src/main.c
+main: run | builds
+	@ builds/out
+
+memchk: memvar = -g -O0
+memchk: run | builds
+	@ valgrind --leak-check=yes ./builds/out
+
+run: ./src/main.c | builds
+	@ gcc -o ./builds/out ./src/main.c $(memvar)
 
 builds:
-	@ mkdir builds
+	mkdir -p $@
--- src/main.c
+++ src/main.c
@@ -14,37 +14,55 @@ str *Str(char *string) {
 
     s->capacity = 2 * strlen(string) * sizeof(char);
     s->literal = malloc(s->capacity);
-    strcpy(string, s->literal);
+    strcpy(s->literal, string);
     s->len = strlen(s->literal);
 
     return s;
 }
 
 char *toCString(str *str) { return str->literal; }
+
 void append(str *string, char *s) {
     if (strlen(s) + string->len >= string->capacity) {
-        string->capacity *= 2;
+        string->capacity = (strlen(s) * sizeof(char)) + string->capacity;
         char *tmp = string->literal;
         string->literal = malloc(string->capacity);
         strcpy(string->literal, tmp);
+        free(tmp);
     }
     strcat(string->literal, s);
     string->len += strlen(s);
-};
+}
 void prepend(str *string, char *s) {
-    char *tmp = string->literal;
+    char *tmp = malloc((string->len + 1) * sizeof(char));
+    strcpy(tmp, string->literal);
     if (strlen(s) + string->len >= string->capacity) {
-        string->capacity *= 2;
+        string->capacity = (strlen(s) * sizeof(char)) + string->capacity;
+        free(string->literal);
         string->literal = malloc(string->capacity);
     }
     memcpy(string->literal, s, (strlen(s) + 1) * sizeof(char));
     strcat(string->literal, tmp);
     string->len += strlen(s);
-};
+    free(tmp);
+}
 void insert(str *str, int pos);
 
 int main() {
-    printf("hi");
+    str *s = Str("Hihhhhhhhh");
+    printf("%s\n", s->literal);
+    printf("%zu/%zu\n", s->len, s->capacity);
+    append(s, " hjjjjjiijjjjjjjjjjjjjjjjjjiiiiiiiiiiiiiiiiiiiiiiiiiii");
+    printf("%s\n", s->literal);
+    printf("%zu/%zu\n", s->len, s->capacity);
+    prepend(s, "Oh ");
+    printf("%s\n", s->literal);
+    printf("%zu/%zu\n", s->len, s->capacity);
+    prepend(s, "1234568888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888887");
+    printf("%s\n", s->literal);
+    printf("%zu/%zu\n", s->len, s->capacity);
+    free(s->literal);
+    free(s);
 
     return 0;
 }
