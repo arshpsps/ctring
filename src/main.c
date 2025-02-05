816a25c655f28ec0d8ee66c82aa27fa118f61015
arshpsps <arshh.gill@outlook.com>
2025-02-05 17:01:24 +0530

    append, prepend

--- src/main.c
+++ src/main.c
@@ -11,7 +11,9 @@ typedef struct {
 
 str *Str(char *string) {
     str *s = malloc(sizeof(str));
-    s->literal = malloc(2 * strlen(string) * sizeof(char));
+
+    s->capacity = 2 * strlen(string) * sizeof(char);
+    s->literal = malloc(s->capacity);
     strcpy(string, s->literal);
     s->len = strlen(s->literal);
 
@@ -19,8 +21,26 @@ str *Str(char *string) {
 }
 
 char *toCString(str *str) { return str->literal; }
-void append(str *string, str s);
-void prepend(str *string, str s);
+void append(str *string, char *s) {
+    if (strlen(s) + string->len >= string->capacity) {
+        string->capacity *= 2;
+        char *tmp = string->literal;
+        string->literal = malloc(string->capacity);
+        strcpy(string->literal, tmp);
+    }
+    strcat(string->literal, s);
+    string->len += strlen(s);
+};
+void prepend(str *string, char *s) {
+    char *tmp = string->literal;
+    if (strlen(s) + string->len >= string->capacity) {
+        string->capacity *= 2;
+        string->literal = malloc(string->capacity);
+    }
+    memcpy(string->literal, s, (strlen(s) + 1) * sizeof(char));
+    strcat(string->literal, tmp);
+    string->len += strlen(s);
+};
 void insert(str *str, int pos);
 
 int main() {
