7fe54eda7e2b59298ff83f838225d46b5c404a58
arshpsps <arshh.gill@outlook.com>
2025-02-11 16:22:25 +0530

    im guessing this is how its done

--- src/ctring.h
+++ src/ctring.h
@@ -3,17 +3,26 @@
 
 #include <stddef.h>
 
-typedef struct {
+typedef struct Ctring {
     char *literal;
     size_t len;
     size_t capacity;
+
+#if defined __cplusplus
+    Ctring &operator[](int pos) {
+        this->literal;
+    }
+
+#endif
 } Ctring;
 
 Ctring *ctring(char *string);
-char *toCString(Ctring *ctring); // FIXME: strcpy and return the pointer to new allocd space
+char *toCString(
+    Ctring *ctring); // FIXME: strcpy and return the pointer to new allocd space
 void append(Ctring *ctring, char *s);
 void prepend(Ctring *ctring, char *s);
 void insert(Ctring *ctring, char *s, int pos);
 int removeFromCtring(Ctring *ctring, size_t pos, size_t size);
+char get(Ctring *ctring, size_t pos);
 
 #endif
